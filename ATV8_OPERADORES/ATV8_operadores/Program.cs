26d85c09908635776e5506a0c29fc6d276e2303b
Messizinh0 <106259319+Messizinh0@users.noreply.github.com>
2022-10-06 10:50:06 -0300

    Add files via upload

---
+++ ATV8_OPERADORES/ATV8_operadores/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+int A = 0;
+int B = 0;
+int VF = 0;
+
+Console.WriteLine("Digite o 1º valor:");
+A = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Digite o 2º valor:");
+B = Convert.ToInt32(Console.ReadLine());
+
+VF = A - B;
+
+if (VF >= 0)
+{
+    Console.WriteLine($"O resultado dos valores digitados é positivo! {VF}");
+}
+
+else
+{
+    Console.WriteLine($"O resultado dos valores digitados é negativo! {VF}");
+}
---
+++ ATV9_operadoresDivisao/ATV9_opdiv/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+int A = 0;
+int B = 0;
+int VF = 0;
+
+Console.WriteLine("Digite dois valores desejados para que seja feito uma divisão e para que seja imprimido o resto da operação");
+
+Console.WriteLine("Digite o 1º valor:");
+A = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Digite o 2º valor:");
+B = Convert.ToInt32(Console.ReadLine());
+
+VF = A % B;
+
+Console.WriteLine($"Veja o que sobrou da operação da divisão: {VF}");
+
